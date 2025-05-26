b392cb656bd74ab708d527dbb056bd982a3f7cde
Mohammad_Fallah <mohammadfallah.w@gmail.com>
2025-05-26 11:42:52 +0330

    feat: :sparkles: Create main grid structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
-import type { Metadata } from "next";
-import { ClerkProvider } from "@clerk/nextjs";
-import "./globals.css";
 import ThemeProvider from "@/components/ThemeProvider";
+import { ClerkProvider } from "@clerk/nextjs";
+import type { Metadata } from "next";
+import "./globals.css";
+import Navbar from "./Navbar";
 
 export const metadata: Metadata = {
   title: "Socially",
@@ -19,7 +20,17 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
             enableSystem
             disableTransitionOnChange
           >
-            {children}
+            <div className="min-h-screen">
+              <Navbar />
+              <main className="py-8">
+                <div className="container mx-auto px-4">
+                  <div className="grid grid-cols-1 lg:grid-cols-12 gap-6">
+                    <div className="hidden lg:block lg:col-span-3">Sidebar</div>
+                    <div className="col-span-1 lg:col-span-9">{children}</div>
+                  </div>
+                </div>
+              </main>
+            </div>
           </ThemeProvider>
         </body>
       </html>
