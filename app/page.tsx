8be81b6c2160d5c38ac7d06792568d9ec4714c4d
Mohammad_Fallah <mohammadfallah.w@gmail.com>
2025-05-27 18:42:21 +0330

    feat: :sparkles: Create home page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
 const HomePage = () => {
-  return <div>Home Page</div>;
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
+      <div className="col-span-1 lg:col-span-6">Create post</div>
+      <div className="hidden lg:block lg:col-span-4 sticky top-20">
+        Who to follow
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
